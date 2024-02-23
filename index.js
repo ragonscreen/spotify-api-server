9d0a57c861ff433b8b5554e79259553d772ad4c3
Anurag Das <ragonscreen@gmail.com>
2024-02-23 19:17:58 +0530

    updated port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
